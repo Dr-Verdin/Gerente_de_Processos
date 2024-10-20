30d8969241fd759d9382066a8801840e1f80c86d
Bruna <moond99999@gmail.com>
2024-10-20 02:23:57 -0300

    update

--- main.c
+++ main.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX 51
+#define MAX_DESCR 51
+#define MAX 100
 
 typedef struct{
     int hh;
@@ -13,13 +14,14 @@ typedef struct{
 typedef struct{
     int prior;
     horario chegada;
-    char descricao[MAX];
+    char descricao[MAX_DESCR];
 }celula;
 
+void leitura_horario();
 void add(celula **prioridade, celula **tempo, int tamanho);
 void exec(celula **prioridade, celula **tempo, int tamanho);
 void next(celula **prioridade, celula **tempo, int tamanho);
-void next(celula **prioridade, celula **tempo, int tamanho);
+void chance(celula **prioridade, celula **tempo, int tamanho);
 void print(celula **prioridade, celula **tempo, int tamanho);
 
 void print(celula **prioridade, celula **tempo, int tamanho){
@@ -28,8 +30,29 @@ void print(celula **prioridade, celula **tempo, int tamanho){
     scanf("%s", flag);
 
     if(strcmp(flag, "-p")){
-        
+        for(int i=tamanho-1; i>=0; i--){
+            celula *aux= prioridade[i];
+            printf("%d %d:%d:%d ", aux->prior, aux->chegada.hh, aux->chegada.mm, aux->chegada.ss);
+            printf("%s", aux->descricao);
+        }
+    }else
+    for(int i=0; i<tamanho; i++){
+        celula *aux = tempo[i];
+        printf("%d %d:%d:%d ", aux->prior, aux->chegada.hh, aux->chegada.mm, aux->chegada.ss);
+        printf("%s", aux->descricao);
+
     }
+}
+
+void add(celula **prioridade, celula **tempo, int tamanho){
+    celula *aux=(celula*)malloc(sizeof(celula));
+
+    scanf("%d", aux->prior);
+    scanf("%d:%d:%d", aux->chegada.hh, aux->chegada.mm, aux->chegada.ss);
+    scanf("%s", aux->descricao);
+
+    
+
 }
 
 
@@ -37,10 +60,13 @@ void print(celula **prioridade, celula **tempo, int tamanho){
 
 int main(void){
     char comando[7];
-    celula **prioridade=(celula**)malloc(sizeof(celula*)*1);
-    celula **tempo=(celula**)malloc(sizeof(celula*)*1);
+    celula **prioridade=(celula**)malloc(sizeof(celula*)*MAX);
+    celula **tempo=(celula**)malloc(sizeof(celula*)*MAX);
     int tamanho=0;
 
+    if(tempo="NULL"){
+        
+    }
 
     while(1){
         scanf("%s", comando);
